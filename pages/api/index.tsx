8b4961e44f5b609168e27fe604eeb4362f97221c
gnehs <jayuiop@gmail.com>
2023-01-12 03:33:25 +0800

    updated style

--- pages/api/index.tsx
+++ pages/api/index.tsx
@@ -2,10 +2,61 @@
 import { ImageResponse } from '@vercel/og';
 import { NextRequest } from 'next/server';
 
+
+const font = fetch(new URL('../../assets/Lato-Regular.ttf', import.meta.url)).then(
+  (res) => res.arrayBuffer(),
+);
+
 export const config = {
   runtime: 'edge',
 };
 export default async function og(req: NextRequest) {
+  const fontData = await font;
+
+  const FooterItem = ({ title, value }) =>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      maxWidth: '33.33%'
+    }}>
+      <div style={{
+        fontSize: 24,
+      }}>{title}</div>
+      <div style={{
+        fontSize: 24,
+        opacity: 0.5,
+        width: '100%'
+      }}>
+        {value || '無'}
+      </div>
+    </div>
+  const Container = ({ children }) =>
+    <div
+      style={{
+        backgroundColor: 'white',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        textAlign: 'left',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        fontFamily: 'Lato',
+        alignItems: 'flex-start',
+        fontSize: 24,
+      }}
+      lang="zh-TW"
+    >{children}</div>
+  const Tag = ({ children }) =>
+    <div style={{
+      fontSize: 24,
+      border: `1px solid #f2f2f2`,
+      padding: '8px 16px',
+      borderRadius: 12,
+      boxShadow: `0 4px 8px rgba(0,0,0,.1)`
+    }}>
+      {children}
+    </div>
+
   const { searchParams } = new URL(req.url)
   const hasYear = searchParams.has('year');
   const hasSem = searchParams.has('sem');
@@ -17,32 +68,19 @@ export default async function og(req: NextRequest) {
     let id = searchParams.get('id');
     const courses = await fetch(`https://gnehs.github.io/ntut-course-crawler-node/${year}/${sem}/${department}.json`).then((res) => res.json());
     const course = courses.find(x => x.id === id);
-    const courseStandardList = {
-      '○': '部訂共同必修',
-      '△': '校訂共同必修',
-      '☆': '共同選修',
-      '●': '部訂專業必修',
-      '▲': '校訂專業必修',
-      '★': '專業選修'
-    }
-    const courseStandard = `📕 ${courseStandardList[course.courseType]}`
     if (course) {
+      const courseStandardList = {
+        '○': '部訂共同必修',
+        '△': '校訂共同必修',
+        '☆': '共同選修',
+        '●': '部訂專業必修',
+        '▲': '校訂專業必修',
+        '★': '專業選修'
+      }
+      const courseStandard = `📕 ${courseStandardList[course.courseType]}`
       return new ImageResponse(
         (
-          <div
-            style={{
-              backgroundColor: 'white',
-              width: '100%',
-              height: '100%',
-              display: 'flex',
-              textAlign: 'left',
-              flexDirection: 'column',
-              justifyContent: 'flex-start',
-              alignItems: 'flex-start',
-              fontSize: 24,
-            }}
-            lang="zh-TW"
-          >
+          <Container>
             <div style={{
               padding: '32px 64px',
               display: 'flex',
@@ -52,7 +90,6 @@ export default async function og(req: NextRequest) {
               <div>🍤 北科課程好朋友</div>
               <div style={{ opacity: .5 }}>ntut-course.gnehs.net</div>
             </div>
-            <div style={{ flex: 1 }} />
             <div style={{
               display: 'flex',
               textAlign: 'left',
@@ -78,20 +115,12 @@ export default async function og(req: NextRequest) {
               <div style={{
                 display: 'flex',
                 gap: 8,
-                marginTop: 8,
+                marginTop: 16,
               }}>
-
-                {[courseStandard, `🎓 ${course.credit} 學分`, ...course.classroom.map(x => `🚪 ${x.name}`)].map(x =>
-                  <div style={{
-                    fontSize: 24,
-                    border: `1px solid #ddd`,
-                    padding: '8px 16px',
-                    borderRadius: 12,
-                  }}
-                    key={x}
-                  >
-                    {x}
-                  </div>
+                <Tag>{courseStandard}</Tag>
+                <Tag>{`${course.credit} 學分`}</Tag>
+                {course.classroom.map(x => `🚪 ${x.name}`).map(x =>
+                  <Tag key={x} > {x} </Tag>
                 )}
               </div>
             </div>
@@ -103,57 +132,23 @@ export default async function og(req: NextRequest) {
               background: '#f2f2f2',
               width: '100%',
             }}>
-              <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-                maxWidth: '33.33%'
-              }}>
-                <div style={{
-                  fontSize: 24,
-                }}>教師</div>
-                <div style={{
-                  fontSize: 24,
-                  opacity: 0.5,
-                  width: '100%'
-                }}>
-                  {course.teacher.map(x => x.name).join('、') || '無'}
-                </div>
-              </div>
-              <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-              }}>
-                <div style={{
-                  fontSize: 24,
-                }}>班級</div>
-                <div style={{
-                  fontSize: 24,
-                  opacity: 0.5,
-                }}>
-                  {course.class.map(x => x.name).join('、') || '無'}
-                </div>
-              </div>
-              <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-              }}>
-                <div style={{
-                  fontSize: 24,
-                }}>備註</div>
-                <div style={{
-                  fontSize: 24,
-                  opacity: 0.5,
-                }}>
-                  {course.notes || '無'}
-                </div>
-              </div>
+              <FooterItem title="教師" value={course.teacher.map(x => x.name).join('、')} />
+              <FooterItem title="班級" value={course.class.map(x => x.name).join('、')} />
+              <FooterItem title="備註" value={course.notes} />
             </div>
-          </div>
+          </Container>
         ),
         {
           width: 1200,
           height: 600,
-          emoji: "fluent"
+          emoji: "fluent",
+          fonts: [
+            {
+              name: 'Lato',
+              data: fontData,
+              style: 'normal',
+            },
+          ],
         },
       );
     }
