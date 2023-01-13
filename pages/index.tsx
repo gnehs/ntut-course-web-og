6d753f1b0c1f2b5eb0155cd3b0f0d839f071a264
gnehs <jayuiop@gmail.com>
2023-01-13 19:12:27 +0800

    updated index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
+
 export default function index() {
   return (
-    <div>北科課程好朋友 OG:image</div>
+    <div>
+      <h1>北科課程好朋友 SNS 預覽圖產生器</h1>
+      <a href="https://github.com/gnehs/ntut-course-web-og">https://github.com/gnehs/ntut-course-web-og</a>
+    </div>
   )
 }
